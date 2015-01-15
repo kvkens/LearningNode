3ec05a16316cf757bd895bd7d1921cabad9f54c5
Kvkens <yueming@qq.com>
2015-01-15 10:24:05 +0800

    update config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {
 	notice : "测试系统开发中……",
 	//site : "http://kvkens-note-pc:3000",
-	site : "http://192.168.9.62",
+	site : "http://192.168.9.123",
 	title : "无与伦比音乐网"
-}
+};
 module.exports = config;
